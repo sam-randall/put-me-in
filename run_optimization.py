4aa6bc71a13cc432d27ea45dc0f5ff16ada4c0b0
Sam Randall <sam.randall5@gmail.com>
2025-02-14 13:17:35 -0800

    prettify debugging process

--- run_optimization.py
+++ run_optimization.py
@@ -1,31 +1,22 @@
 import cvxpy as cp
 import numpy as np
+import pandas as pd
 
 def main():
     NUMBER_OF_PERIODS = 6
     NUMBER_OF_CHILDREN = 10
     MAX_CONSECUTIVE_BENCH_PERIODS = 1
     N_ALL_STAR_PERIODS = 1
+    SPOTS = 5
 
     skills = np.array([2, 2, 2, 3, 3, 3, 4, 4, 5, 7])
 
-    SPOTS = 5
-
     best_player_indices = np.argsort(skills)[-SPOTS:]
-    print(best_player_indices)
 
     # We can specify a matrix here.
     variables = cp.Variable((NUMBER_OF_PERIODS, NUMBER_OF_CHILDREN), boolean = True)
-    # total = 0
 
-    print(variables)
-
-    # for i in range(len(variables)):
-        # total += skills @ variables[i]
-
-    # mu = total / NUMBER_OF_PERIODS
-    # TODO: Compute variance. 
-
+    # TODO: Minimize variance. 
     objective = cp.Minimize(1)
 
     constraints = []
@@ -35,7 +26,6 @@ def main():
     # playing is the number allowed.
     for p in range(NUMBER_OF_PERIODS):
         constraints += [variables[p].sum() == SPOTS]
-
 
     # For each child, make sure it never has more than 
     # MAX_CONSECUTIVE_BENCH_PERIODS in a row.
@@ -60,8 +50,6 @@ def main():
 
     M = 1000  # Large enough to handle constraints but not too large
 
-    print("max skill", max_skill)
-
     constraints += [period_totals == variables @ skills - max_skill]
     # Constraints
     constraints_all_star = [
@@ -69,26 +57,33 @@ def main():
         period_totals >= -M * (1 - indicator),  # Lower bound when indicator=1
         cp.sum(indicator) == N_ALL_STAR_PERIODS  # Ensure exactly N_ALL_STAR_PERIODS are max_skill
     ]
+    # TODO: Indicator doesn't have enough forced ones. 
+
     constraints += constraints_all_star
-
-    
-
-    
 
     problem = cp.Problem(objective, constraints)
     problem.solve()
 
+    print("Assignments.")
+    df = pd.DataFrame(variables.value, columns = [f'player_{i}' for i in range(NUMBER_OF_CHILDREN)])
+    df.index = [f"Period {p}" for p in range(NUMBER_OF_PERIODS)]
+    print(df)
+    print()
 
-    print(variables.value)
 
     print("Derived Variables.")
 
     print("Period Totals", period_totals.value)
     print("Indicator", indicator.value)
+    print()
 
     print("Constraints.")
 
     print(period_totals.value, '<=', M * (1 - indicator.value))
     print(period_totals.value, '>=',-M * (1 - indicator.value))
+
+
+    # Current Problem: 
+
 if __name__ == '__main__':
     main()
