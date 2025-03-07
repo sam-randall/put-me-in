01b7d472b4e4266f763be58ab18ee72fc33e6c14
Sam Randall <sam.randall5@gmail.com>
2025-03-06 23:57:21 -0800

    Remove Unused Import

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List, Optional, Union
+from typing import List, Union
 
 from fastapi.middleware.cors import CORSMiddleware
 from run_optimization import generate_assignment, Game
