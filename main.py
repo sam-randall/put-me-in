7601967232af9c456c7c11fedd7590bdf853a1e3
Sam Randall <sam.randall5@gmail.com>
2025-04-17 23:14:59 -0700

    Add Num Periods

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from typing import List, Union
 from fastapi.middleware.cors import CORSMiddleware
 from run_optimization import generate_assignment, Game
 
-
+import logging
 app = FastAPI()
+
+logger = logging.getLogger('uvicorn.error')
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,17 +25,22 @@ class Player(BaseModel):
     skill_level: float
 
 
+class LineupRequest(BaseModel):
+    players: List[Player]
+    initial_lineup: Union[List[str], str] = 'auto'
+    num_periods: int = 6
+
 
 @app.post("/submit_lineup/")
-async def submit_lineup(players: List[Player], initial_lineup: Union[List[str], str] = 'auto', num_periods: int = 6):
-    print("Players", players)
-    print("Lineup", initial_lineup)
-    game = Game(num_periods, 5)
-    names = [player.name for player in players]
-    skill_level = [player.skill_level for player in players]
-    out, status = generate_assignment(names, skill_level, game, initial_lineup)
+async def submit_lineup(request: LineupRequest):
+    logging.info(request)
+
+    game = Game(request.num_periods, 5)
+    names = [player.name for player in request.players]
+    skill_level = [player.skill_level for player in request.players]
+    out, status = generate_assignment(names, skill_level, game, request.initial_lineup)
     out = out.T.to_html()
-    return {"vis": out, "optimization_status": status, 'num_periods': num_periods}
+    return {"vis": out, "optimization_status": status, 'num_periods': request.num_periods}
 
 # Run the server 
 # Run this in the terminal: uvicorn main:app --reload
